4f3bdc57ef001828935688762829db5d49453a6d
sunilkumargavara <sunilkumar_gavara@epam.com>
2021-10-27 11:35:54 +0530

    1. Add course controller 2. Add re direction in bootcamp route for courses 3. Add Course Model and Course Route 4. Add the route inside server file

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,7 +9,14 @@ const {
   getBootCampsInRadius
 } = require('../controllers/bootcamps')
 
+// Include other resourse routers
+const courseRouter = require('./courses')
+
 const router = express.Router()
+
+// Re route into other resourse
+router.use('/:bootcampId/courses', courseRouter)
+
 
 router.route('/').get(getBootcamps).post(createBootcamp)
 
--- seeder.js
+++ seeder.js
@@ -8,17 +8,20 @@ dotEnv.config({path: "./config/config.env"})
 
 //Load Models
 const Bootcamp = require('./models/Bootcamp')
+const Course = require('./models/Course')
 
 // Conncet DB
 mongoose.connect(process.env.MONGO_URI)
 
 // Read JSON Files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
 
 // IMPORT into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
+        await Course.create(courses)
         console.log("Data Imported....".green.inverse)
         process.exit()
     } catch(err) {
@@ -31,6 +34,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
+        await Course.deleteMany()
         console.log("Data Distroyed.....".red.inverse)
         process.exit()
     } catch(err) {
