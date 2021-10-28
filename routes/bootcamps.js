e697d63eadffd9e3e2d4dc115d7ec6016b0e41d2
sunilkumargavara <sunilkumar_gavara@epam.com>
2021-10-28 14:58:33 +0530

    1. Add missed CRUD routes in course conroller 2. Add advanced filtering middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 
 const {
   getBootcamps,
@@ -6,22 +6,35 @@ const {
   updateBootcamp,
   deleteBootcamp,
   createBootcamp,
-  getBootCampsInRadius
-} = require('../controllers/bootcamps')
+  getBootCampsInRadius,
+  bootcampPhotoUpload,
+} = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+
+const advancedResults = require('../mideleware/advancedResults');
 
 // Include other resourse routers
-const courseRouter = require('./courses')
+const courseRouter = require('./courses');
 
-const router = express.Router()
+const router = express.Router();
 
 // Re route into other resourse
-router.use('/:bootcampId/courses', courseRouter)
+router.use('/:bootcampId/courses', courseRouter);
 
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 
-router.route('/').get(getBootcamps).post(createBootcamp)
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
-router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
+router.route('/radius/:zipcode/:distance').get(getBootCampsInRadius);
 
-router.route('/radius/:zipcode/:distance').get(getBootCampsInRadius)
+router.route('/:id/photo').put(bootcampPhotoUpload);
 
-module.exports = router
+module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,26 @@
 const express = require('express');
 
-const { getCourses } = require('../controllers/courses')
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} = require('../controllers/courses');
 
-const router = express.Router({mergeParams: true})
+const advancedResult = require('../mideleware/advancedResults');
+const Course = require('../models/Course');
 
-router.route('/').get(getCourses)
+const router = express.Router({ mergeParams: true });
 
-module.exports = router
+router
+  .route('/')
+  .get(
+    advancedResult(Course, { path: 'bootcamp', select: 'name description' }),
+    getCourses
+  )
+  .post(addCourse);
+
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+
+module.exports = router;
